d4c63b9158af27acdc976a53d5456aa59351d297
shentao <shentao@elex-tech.com>
2026-01-13 09:50:59 +0800

    Docs: code clean

--- Runtime/Graphics/MeshUtils.cpp
+++ Runtime/Graphics/MeshUtils.cpp
@@ -5,18 +5,18 @@
 namespace EngineCore
 {
 	// todo： vertex数据结构优化， 不需要三个数据
-	void MeshUtils::GetFullScreenQuad(Mesh* modelData)
+	void MeshUtils::GetFullScreenQuad(Mesh* mesh)
 	{
-		modelData->layout.emplace_back(InputLayout(VertexAttribute::POSITION, 3 * sizeof(float), 3, 8 * sizeof(float), 0));
-		modelData->layout.emplace_back(InputLayout(VertexAttribute::NORMAL, 3 * sizeof(float), 3, 8 * sizeof(float), 3 * sizeof(float)));
-		modelData->layout.emplace_back(InputLayout(VertexAttribute::UV0, 2 * sizeof(float), 2, 8 * sizeof(float), 6 * sizeof(float)));
-		modelData->index.insert(modelData->index.end(), { 0,1,2,1,3,2 });
-		modelData->vertex.insert(modelData->vertex.end(), {
+		mesh->layout.emplace_back(InputLayout(VertexAttribute::POSITION, 3 * sizeof(float), 3, 8 * sizeof(float), 0));
+		mesh->layout.emplace_back(InputLayout(VertexAttribute::NORMAL, 3 * sizeof(float), 3, 8 * sizeof(float), 3 * sizeof(float)));
+		mesh->layout.emplace_back(InputLayout(VertexAttribute::UV0, 2 * sizeof(float), 2, 8 * sizeof(float), 6 * sizeof(float)));
+		mesh->index.insert(mesh->index.end(), { 0,1,2,1,3,2 });
+		mesh->vertex.insert(mesh->vertex.end(), {
 			Vertex{Vector3(-1,-1,0), Vector3(0,0,1), Vector2(0,0)},  // 左下
 			Vertex{Vector3(-1, 1,0), Vector3(0,0,1), Vector2(0,1)},  // 左上  
 			Vertex{Vector3(1,-1,0), Vector3(0,0,1), Vector2(1,0)},  // 右下
 			Vertex{Vector3(1, 1,0), Vector3(0,0,1), Vector2(1,1)}   // 右上
 			});
-		modelData->UploadMeshToGPU();
+		mesh->UploadMeshToGPU();
 	}
 }
