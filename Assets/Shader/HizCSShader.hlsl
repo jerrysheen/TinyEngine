7cf62ff88b356453440081fecdf0e6c828dee1ba
shentao <shentao@elex-tech.com>
2026-04-19 18:47:01 +0800

    D： 为pc端增加D32S8类型， GPUScene可能还是存在精度问题需要调整，跑通基本GPUScene

--- Assets/Shader/HizCSShader.hlsl
+++ Assets/Shader/HizCSShader.hlsl
@@ -1,63 +1,36 @@
-struct IndirectDrawDest
+RWTexture2D<float> gInputTexture  : register(u0, space1);
+RWTexture2D<float> gOutputTexture : register(u1, space1);
+
+[numthreads(8, 8, 1)]
+void CSMain(uint3 DTid : SV_DispatchThreadID)
 {
-    uint StartIndexInVisibilityBuffer;
-    uint IndexCountPerInstance;
-    uint InstanceCount;
-    uint StartIndexLocation;
-    uint BaseVertexLocation;
-    uint StartInstanceLocation;
-};
+    uint inputWidth, inputHeight;
+    gInputTexture.GetDimensions(inputWidth, inputHeight);
 
+    uint outputWidth, outputHeight;
+    gOutputTexture.GetDimensions(outputWidth, outputHeight);
 
-struct IndirectDrawSrouce
-{
-    uint StartIndexInVisibilityBuffer;
-    uint IndexCountPerInstance;
-    uint InstanceCount;
-    uint StartIndexLocation;
-    uint BaseVertexLocation;
-    uint StartInstanceLocation;
-    //  记录当前这个IndirectDrawSource在哪个pass里面
-    uint PassIndex;
-};
+    if (DTid.x >= outputWidth || DTid.y >= outputHeight)
+    {
+        return;
+    }
 
+    uint2 coord = DTid.xy * 2;
 
-cbuffer BatchCountBuffer : register(b0)
-{
-    uint bacthCount; 
-};
+    uint2 maxCoord = uint2(inputWidth - 1, inputHeight - 1);
+    uint2 coord00 = min(coord, maxCoord);
+    uint2 coord10 = min(coord + uint2(1, 0), maxCoord);
+    uint2 coord01 = min(coord + uint2(0, 1), maxCoord);
+    uint2 coord11 = min(coord + uint2(1, 1), maxCoord);
 
-// ==========================================
-// 资源绑定
-// ==========================================
+    float d00 = gInputTexture[coord00];
+    float d10 = gInputTexture[coord10];
+    float d01 = gInputTexture[coord01];
+    float d11 = gInputTexture[coord11];
 
-// 输入：
-RWStructuredBuffer<IndirectDrawSrouce> IndirectDrawSourceBuffer : register(u0); // 手动传入一个计数器 buffer
+    // 如果 Reverse-Z 是 near=1 far=0，则 HiZ 保守值通常取 max。
+    // 如果你当前 depth 约定真的是 0 最近 1 最远，才取 min。
+    float hizDepth = min(min(d00, d10), min(d01, d11));
 
-
-// 输出：IndirectDrawCall
-RWStructuredBuffer<IndirectDrawDest> IndirectDrawDestBuffer : register(u1); // 手动传入一个计数器 buffer
-RWStructuredBuffer<uint> IndirectDrawCount : register(u2); // 手动传入一个计数器 buffer
-
-// ===================================  ======
-// 主函数
-// ==========================================
-// 线程组大小：通常设为 64 或 128
-[numthreads(64, 1, 1)]
-void CSMain(uint3 dtid : SV_DispatchThreadID)
-{
-    if(dtid.x > bacthCount) return;
-    uint instanceIndex = dtid.x;
-    uint passIndex = IndirectDrawSourceBuffer[instanceIndex].PassIndex;
-    if(IndirectDrawSourceBuffer[instanceIndex].InstanceCount == 0) return;
-    uint instanceCount;
-    InterlockedAdd(IndirectDrawCount[passIndex], 1, instanceCount);
-    instanceCount = instanceCount + passIndex * 1000;
-    IndirectDrawDestBuffer[instanceCount].StartIndexInVisibilityBuffer = IndirectDrawSourceBuffer[instanceIndex].StartIndexInVisibilityBuffer;
-    IndirectDrawDestBuffer[instanceCount].IndexCountPerInstance = IndirectDrawSourceBuffer[instanceIndex].IndexCountPerInstance;
-    IndirectDrawDestBuffer[instanceCount].InstanceCount = IndirectDrawSourceBuffer[instanceIndex].InstanceCount;
-    IndirectDrawDestBuffer[instanceCount].StartIndexLocation = IndirectDrawSourceBuffer[instanceIndex].StartIndexLocation;
-    IndirectDrawDestBuffer[instanceCount].BaseVertexLocation = IndirectDrawSourceBuffer[instanceIndex].BaseVertexLocation;
-    IndirectDrawDestBuffer[instanceCount].StartInstanceLocation = IndirectDrawSourceBuffer[instanceIndex].StartInstanceLocation;
-
+    gOutputTexture[DTid.xy] = hizDepth;
 }
